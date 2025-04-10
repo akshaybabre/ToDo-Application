000d224dbd9116b766b7f83207ec02f391c2a35a
akshaybabre <akshaybabre8@gmail.com>
2025-04-11 00:21:38 +0530

    Fixed wildcard route for Express 5 compatibility

--- api.js
+++ api.js
@@ -134,7 +134,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get('/:catchAll', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
